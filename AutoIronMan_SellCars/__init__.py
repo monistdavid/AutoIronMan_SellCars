7c74e7a649a3fdcea34874888ff98925c0b8c93d
xiang-ya <davidhoneu@gmail.com>
2023-08-04 20:27:50 -0400

    update the whole project to a better Flask structure

--- AutoIronMan_SellCars/__init__.py
+++ AutoIronMan_SellCars/__init__.py
@@ -16,8 +16,8 @@ def create_app():
     return app
 
 
-app = create_app()
-app.secret_key = "monist"
+sellCarsApp = create_app()
+sellCarsApp.secret_key = "monist"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    sellCarsApp.run(host='0.0.0.0', port=8000)
